d1cb8eee367eecbe1a149741201aab68b8d01a16
chinsinong <chinsin.ong@gmail.com>
2020-11-06 22:27:18 +0800

    add item remove item approve rfq

--- RFQ.sol
+++ RFQ.sol
@@ -15,8 +15,9 @@ contract RFQ {
     address private selectedQuotation;
     
     string[] private item;
-    
-    uint16[] private quantity;
+
+    mapping(string => uint) quantity;
+
     
     bool private statusApproved = false;
     
@@ -26,14 +27,14 @@ contract RFQ {
     
     constructor (
         
-        address requestorVal
+        address requestorVal,
+        address approverVal
         
     ) public {
-        
-        require(tx.origin == requestorVal, "Only requestor can create RFQ.");
+        require(requestorVal != approverVal, "Requestor and approver cannot be same person.");
         
         requestor = requestorVal;
-        
+        approver = approverVal;
     }
     
     function getInfo() public view returns (
@@ -41,29 +42,58 @@ contract RFQ {
         address approverVal,
         address[] supplierVal,
         string[] itemVal,
-        uint16[] quantityVal
+        uint[] quantityVal
     ) {
-        return (requestor, approver, supplier, item, quantity);
+        
+        uint[] memory qty = new uint[](item.length);
+        
+        for(uint i=0; i < item.length; i++) {
+            qty[i] = quantity[item[i]];
+        }
+        
+        return (requestor, approver, supplier, item, qty);
     }
     
-    function setApprover(address approverVal) public {
-        require(requestor != approverVal, "Requestor cannot be approver");
-        approver = approverVal;
-    }
     
-    function setStatusApprove(bool approverVal) public {
+    function approveRFQ() public {
+        require(statusApproved == false, "RFQ is already approved.");
         require(msg.sender == approver, "Only approver can approve the RFQ");
         statusApproved = true;
     }
     
-    function addProduct(string itemVal, uint16 quantityVal) public {
+    function additem(string itemVal, uint quantityVal) public {
         require(statusApproved == false, "No item can be added after RFQ is approved.");
-        require(msg.sender == requestor, "Only requestor can add product.");
+        require(msg.sender == requestor || msg.sender == approver, "Only requestor or approver can add product.");
         
-        item.push(itemVal);
-        quantity.push(quantityVal);
+        
+        if(quantity[itemVal] == 0) {
+            item.push(itemVal);
+        }
+        quantity[itemVal] += quantityVal;
     }
     
-    
+    function removeitem(string itemVal, uint quantityVal) public {
+        require(statusApproved == false, "No item can be removed after RFQ is approved.");
+        require(msg.sender == requestor || msg.sender == approver, "Only requestor or approver can remove product.");
+        require(quantity[itemVal] > 0 , "Item does not exist in RFQ.");
+        require(quantityVal <= quantity[itemVal], "Quantity must be less than existing quantity.");
+
+        if(quantityVal == quantity[itemVal]) {
+            for(uint i=0; i < item.length; i++) {
+                
+                if(keccak256(bytes(itemVal)) == keccak256(bytes(item[i]))) {
+                    
+                    for(uint j=i; j < item.length - 1; j++) {
+                        item[j] = item[j+1];
+                    }
+                    delete item[item.length - 1];
+                    item.length--;
+                    break;
+                }
+            }
+        }
+        
+        quantity[itemVal] -= quantityVal;
+    }
     
 }
